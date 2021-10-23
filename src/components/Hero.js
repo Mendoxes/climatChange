cc05ce928ea6a1ecd2220ba7868e8de42add682e
Sebastian <seba.lewy@op.pl>
2021-10-23 14:36:16 +0100

    change

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,12 +8,16 @@ import { height } from 'dom-helpers';
 import FadeIn from 'react-fade-in'
 import RenderIfVisible from 'react-render-if-visible'
 import Fade from 'react-reveal/Fade';
-
-
+import logo from "../warning.png"
+import hot from "../hot.png"
+import tsunami from "../tsunami.png"
+import arctic from "../arctic.png"
+import hope from "../hope.jpg"
 
 export default function Hero() {
 
 const [fade1, setFade1] = useState(true)
+const [fade2, setFade2] = useState(true)
 
   const VisibilitySensor = require('react-visibility-sensor');
 
@@ -89,7 +93,7 @@ One World
       };
 
       if (!state){
-        setTimeout(function(){  document.querySelector(".scroling").scrollIntoView({ block: "end",behavior: 'smooth' }) }, 6000);
+        setTimeout(function(){  document.querySelector(".App-header").scrollIntoView({ block: "end",behavior: 'smooth' }) }, 6000);
        
       }
     
@@ -113,21 +117,61 @@ One World
         </div>}
       
 {/* <div ></div> */}
+
+<div className="info"> 
+
+<div className="small">
+
+
+  <div className="smaller1">Carbon dioxide:</div>
+
+<img className="smallerIMG" src={logo} alt="Logo" />
+  <div className="smaller2">417</div>
+
+  <div className="smaller3">parts per milion</div>
+</div>
+<div className="small">
+
+
+  <div className="smaller1">Global Temperature risen:</div>
+  <img className="smallerIMG" src={hot} alt="Logo" />
+  <div className="smaller2">1.8C</div>
+  <div className="smaller3">since 1880</div>
+</div>
+<div className="small">
+
+
+  <div className="smaller1">Sea level risen:</div>
+  <img className="smallerIMG" src={tsunami} alt="Logo" />
+  <div className="smaller2">3.4</div>
+  <div className="smaller3">minimeters per year</div>
+</div>
+<div className="small">
+
+
+  <div className="smaller1">Arctic ice decreased:</div>
+  <img className="smallerIMG" src={arctic} alt="Logo" />
+  <div className="smaller2">13.2</div>
+  <div className="smaller3">percent per decade</div>
+</div>
+</div>
 <Fade >
       <div className="scroling"> 
       <div  className="scroling2">
      
       
        
-      {fade1&& <div className="common third"> <Fade delay={399}><h1 onMouseEnter ={()=>setFade1(false)} className="fonts"> Evidence</h1></Fade>
+      {fade1&& <div className="common third"> <Fade delay={99}><h1 onMouseEnter ={()=>setFade1(false)} className="fonts"> Evidence</h1></Fade>
         
-        {!fade1&&<Fade ><h1  onMouseLeave={()=>setFade1(true)} className="fonts2"> How do we know climate change is real?</h1></Fade>}
+       
         
         </div>}
         
         {!fade1&&<div className="common third2"><Fade ><h1  onMouseLeave={()=>setFade1(true)} className="fonts2"> How do we know climate change is real?</h1></Fade></div>}
         
-        <div  className="common first"><Fade delay={599}><h1 className="fonts"> How?</h1></Fade></div>
+        {fade2&&<div  className="common first" onMouseEnter ={()=>setFade2(false)}><Fade delay={129}><h1  className="fonts"> How?</h1></Fade></div>}
+
+        {!fade2&&<div  className="common first2" onMouseLeave={()=>setFade2(true)} ><Fade delay={99}><h1  className="fonts2">What can be done to solve climate change?</h1></Fade></div>}
         <div className="common second"><Fade delay={999}><h1 className="fonts"> What if?</h1></Fade></div>
         </div>
 
@@ -144,9 +188,12 @@ One World
 
 <Fade>{/*Using Zoom Effect*/}
           <header className="App-header">
-          
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
+            <div className="head-text">
+       <img className="smallerIMG2" src={hope} alt="ok"></img>
+     <Fade>  <div className="text-on-image"> There is still hope</div></Fade>
+       </div>
+
+       </header>
         </Fade>
 
 
